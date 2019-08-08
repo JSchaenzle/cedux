6461cabf0ce1d0e71d811abebd31ed87f9b1f750
Jordan Schaenzle <jordan.x.schaenzle@gsk.com>
2019-08-08 15:10:30 -0400

    Updated unit tests

--- test/support/cedux_test_helpers.h
+++ test/support/cedux_test_helpers.h
@@ -3,7 +3,9 @@
 #include <stdbool.h>
 
 bool mockable_reducer1(int * p_state, int action);
+bool mockable_reducer2(int * p_state, int action);
 // p_store is a void pointer here because we don't have access to the correct store handle type... We'll have to use a cast in the test when registering this.
 void mockable_subscriber1(void * p_store, int const * const p_state, void * p_data);
+void mockable_subscriber2(void * p_store, int const * const p_state, void * p_data);
 
 #endif
